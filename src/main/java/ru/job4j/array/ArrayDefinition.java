6fb762111323a1549c37a282b9a2a3e6ecfd00ba
Tamerlanaz <tamerlanaz@gmail.com>
2022-04-26 20:00:29 +0300

    6.4.1.1. Обход массива с последнего элемента [#361619]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -6,9 +6,9 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new  float[40];
-        System.out.println("п═п╟п╥п╪п╣я─ п╪п╟я│я│п╦п╡п╟ ages я─п╟п╡п╣п╫ - " + ages.length);
-        System.out.println("п═п╟п╥п╪п╣я─ п╪п╟я│я│п╦п╡п╟ surnames я─п╟п╡п╣п╫ - " + surnames.length);
-        System.out.println("п═п╟п╥п╪п╣я─ п╪п╟я│я│п╦п╡п╟ prices я─п╟п╡п╣п╫ - " + prices.length);
+        System.out.println("Размер массива ages равен - " + ages.length);
+        System.out.println("Размер массива surnames равен - " + surnames.length);
+        System.out.println("Размер массива prices равен - " + prices.length);
 
     }
 }
