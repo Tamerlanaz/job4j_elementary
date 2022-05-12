f4885171ee066a5c6ed442e3d44adbc9f2c363b1
Tamerlanaz <tamerlanaz@gmail.com>
2022-05-12 07:55:13 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/math/condition/Max.java
+++ src/main/java/ru/job4j/math/condition/Max.java
@@ -2,7 +2,16 @@ package ru.job4j.math.condition;
 
 public class Max {
 
-    public static int max(int left, int right) {
-        return left >= right ? left : right;
+    public static int max(int one, int two) {
+        return one >= two ? one : two;
+    }
+
+    public static int max(int one, int two, int three) {
+       int tmp = one >= two ? one : two;
+       return three >= tmp ? three : tmp;
+    }
+
+    public static int max(int one, int two, int three, int four) {
+        return four >= max(one, two, three) ? four : max(one, two, three);
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -4,13 +4,37 @@ import org.junit.Assert;
 import org.junit.Test;
 import ru.job4j.math.condition.Max;
 
+import static ru.job4j.math.condition.Max.*;
+
 public class MaxTest {
+    @SuppressWarnings("checkstyle:WhitespaceAfter")
     @Test
     public void whenMax1To2Then2() {
-        int left = 1;
-        int right = 2;
-        int result = Max.max(left, right);
+        int one = 1;
+        int two = 2;
+        int result = Max.max(one, two);
         int expected = 2;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax5To2To3Then5() {
+        int one = 5;
+        int two = 2;
+        int three = 3;
+        int result = Max.max(one, two, three);
+        int expected = 5;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax1To2To3To4Then4() {
+        int one = 1;
+        int two = 2;
+        int three = 3;
+        int four = 4;
+        int result = Max.max(one, two, three, four);
+        int expected = 4;
         Assert.assertEquals(result, expected);
     }
 }
